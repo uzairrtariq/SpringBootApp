1f428a79472bc1bcf7e23828588731441c3b63f1
Uzair Tariq <Tariquu@hendrix.edu>
2020-10-15 23:21:16 +0500

    Added a Spring boot project
    Added a basic skeleton of Spring project thru spring initializer.

---
+++ restful-web-service/src/test/java/com/digitifyassignment/rest/webservices/restfulwebservice/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitifyassignment.rest.webservices.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
